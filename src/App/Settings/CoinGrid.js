6f834b3ac724a246bab45e4e611a0307d9dd3108
Pietrzi <pietrzi@op.pl>
2020-02-22 18:40:12 +0100

    coins almost ready

--- src/App/Settings/CoinGrid.js
+++ src/App/Settings/CoinGrid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../AppProvider';
-import {SelectableTile} from "../Shared/Tile";
+import CoinTile from "./CoinTile";
 
 
 export const CoinGridStyled = styled.div`
@@ -10,12 +10,17 @@ grid-template-columns: repeat(5, 1fr);
 grid-gap: 15px;
 `
 
+function getCoinsToDisplay(coinList) {
+    return Object.keys(coinList).slice(0, 100);
+}
+
 export default function() {
     return (
         <AppContext.Consumer>
             {({coinList})=> <CoinGridStyled>
-                                {Object.keys(coinList).map(coinKey =>
-                                    <SelectableTile> {coinKey} </SelectableTile>)}
+                                {getCoinsToDisplay(coinList).map(coinKey =>
+                                    <CoinTile coinKey={coinKey}/>
+                                )}
                             </CoinGridStyled>}
         </AppContext.Consumer>
     )
