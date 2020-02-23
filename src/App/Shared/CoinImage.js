2206d118fd2b61538e0bc630aee269d2e3d55321
Pietrzi <pietrzi@op.pl>
2020-02-23 19:40:29 +0100

    aded search

--- src/App/Shared/CoinImage.js
+++ src/App/Shared/CoinImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function ({coin, style}) {
-    return <img
+    return !coin ? <div>No coin data found </div> : <img
       alt={coin.CoinSymbol}
       style={style || {height: '50px'}}
       src={`http://cryptocompare.com/${
